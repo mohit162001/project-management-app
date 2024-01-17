13cb439ada9d4ad79151019452ac4e4b36dca9e3
Mohit Devade <mohit.deavade123@gmail.com>
2024-01-17 19:27:53 +0530

    side bar component added

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './sidebar.css'
+import Button from './Button';
+function SideBar({handleSelectedProject}) {
+  return (
+    <aside>
+        <h2>Your Projects</h2>
+        <div>
+            <Button onClick={handleSelectedProject}>Add Project</Button>
+        </div>
+        <ul></ul>
+    </aside>
+  )
+}
+
+export default SideBar;
